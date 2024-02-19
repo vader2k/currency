80c3cf8e3a131507769154362b873b9203bc821c
danie odukwe <kingdanny295@gmail.com>
2024-02-19 13:59:23 +0100

    done with line charts

--- src/pages/CryptoDetails.jsx
+++ src/pages/CryptoDetails.jsx
@@ -17,8 +17,7 @@ const CryptoDetails = () => {
   const { data: coinHistory } = useGetCryptoHistoryQuery({coinId, timePeriod})
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId)
   const cryptoDetails = data?.data?.coin
-  console.log(coinHistory)
-
+ 
   if (isFetching) return "loading..."
 
   const time = [ '3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y' ]
